60dc188731eaf1a1364074b78733c596f86d40d8
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-12-26 08:36:22 +0200

    Add main

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+author : Abd El-Twab M. Fakhry
+created: 24-Dec-2020
+"""
+
+from Windows import *
+from tkinter import *
+
+windows = Tk()
+windows.title("Queueing ModelSim")
+windows.geometry("747x385")
+
+QC = Windows(windows)
+windows.mainloop()
