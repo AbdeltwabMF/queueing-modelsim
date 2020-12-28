a1ae4e772aca34655f9f63526392ed6aef47df9c
Abd El-Twab  M. Fakhry <abdeltwab.m.fakhry@gmail.com>
2020-12-28 13:32:01 +0200

    Change x and y limits

--- src/Plots.py
+++ src/Plots.py
@@ -10,10 +10,10 @@ class Plots:
 
 		plt.xlabel("t(s)", fontsize=16, loc="right", color="#ff0033")
 		plt.ylabel("n(t)", fontsize=16, rotation=0, loc="top", color="#ff0033")
-		plt.title("Customers per Second", fontsize=16, color="#220066")
+		plt.title("Customers in System per Second", fontsize=16, color="#220066")
 
-		plt.ylim(0, dd1k.get_k() + dd1k.get_initial_customers() + 1)
-		plt.xlim(0, 70)
+		plt.ylim(0, dd1k.get_k() + dd1k.get_initial_customers() + 2)
+		plt.xlim(0, 60)
 
 		plt.step(range(len(dd1k.system_size_per_time)), dd1k.system_size_per_time, color="#cc0033", linewidth=3)
 
@@ -25,14 +25,14 @@ class Plots:
 		plt.grid()
 		# plt.style.use("fivethirtyeight")
 
-		plt.xlabel("Customer", fontsize=16, loc="right", color="#ff0033")
-		plt.ylabel("departure time", fontsize=16, rotation=0, loc="top", color="#ff0033")
+		plt.xlabel("Customer (n)", fontsize=16, loc="center", color="#ff0033")
+		plt.ylabel("Time (s)", fontsize=16, rotation=90, loc="center", color="#ff0033")
 		plt.title("Departure Time per Customer", fontsize=16, color="#220066")
 
-		plt.ylim(0, dd1k.get_k() + dd1k.get_initial_customers() + 1)
-		plt.xlim(0, 70)
+		plt.ylim(0, max(dd1k.departures[:30]) + 2)
+		plt.xlim(0, 30)
 
-		plt.step(range(len(dd1k.departures)), dd1k.departures, color="#00ffcc", linewidth=3)
+		plt.step(range(len(dd1k.departures)), dd1k.departures, color="#cc0033", linewidth=3)
 
 		plt.show()
 
@@ -42,14 +42,14 @@ class Plots:
 		plt.grid()
 		# plt.style.use("fivethirtyeight")
 
-		plt.xlabel("Customer", fontsize=16, loc="right", color="#ff0033")
-		plt.ylabel("queue time", fontsize=16, rotation=0, loc="top", color="#ff0033")
-		plt.title("Queue Time per Customer", fontsize=16, color="#220066")
+		plt.xlabel("Customer (n)", fontsize=16, loc="center", color="#ff0033")
+		plt.ylabel("Time (s)", fontsize=16, rotation=90, loc="center", color="#ff0033")
+		plt.title("Time Spent in Queue per Customer", fontsize=16, color="#220066")
 
-		plt.ylim(0, dd1k.get_k() + dd1k.get_initial_customers() + 1)
-		plt.xlim(0, 70)
+		plt.ylim(0, max(dd1k.queue_time[:30]) + 2)
+		plt.xlim(0, 30)
 
-		plt.step(range(len(dd1k.queue_time)), dd1k.queue_time, color="#0000cc", linewidth=3)
+		plt.step(range(len(dd1k.queue_time)), dd1k.queue_time, color="#cc0033", linewidth=3)
 
 		plt.show()
 
@@ -59,13 +59,13 @@ class Plots:
 		plt.grid()
 		# plt.style.use("fivethirtyeight")
 
-		plt.xlabel("Customer", fontsize=16, loc="right", color="#ff0033")
-		plt.ylabel("total time", fontsize=16, rotation=0, loc="top", color="#ff0033")
-		plt.title("Total time in the System per Customer", fontsize=16, color="#220066")
+		plt.xlabel("Customer (n)", fontsize=16, loc="center", color="#ff0033")
+		plt.ylabel("Time (s)", fontsize=16, rotation=90, loc="center", color="#ff0033")
+		plt.title("Time Spent in System per Customer", fontsize=16, color="#220066")
 
-		plt.ylim(0, dd1k.get_k() + dd1k.get_initial_customers() + 1)
-		plt.xlim(0, 70)
+		plt.ylim(0, max(dd1k.total_time[:30]) + 2)
+		plt.xlim(0, 30)
 
-		plt.step(range(len(dd1k.total_time)), dd1k.total_time, color="#3399ff", linewidth=3)
+		plt.step(range(len(dd1k.total_time)), dd1k.total_time, color="#cc0033", linewidth=3)
 
 		plt.show()
